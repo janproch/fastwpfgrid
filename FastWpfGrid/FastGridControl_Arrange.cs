0e702bc945a8fcbf0178e54dcdac0b34d221728a
victor gallipoli <victorgallipoli@gmail.com>
2022-02-04 15:12:21 +0100

    Fix round columnsize to dpi pixel size

--- FastWpfGrid/FastGridControl_Arrange.cs
+++ FastWpfGrid/FastGridControl_Arrange.cs
@@ -476,7 +476,8 @@ namespace FastWpfGrid
 
             for (var i = 0; i < sizes.Length; i++)
             {
-                _columnSizes.PutSizeOverride(i, sizes[i]);
+                var pixelSize = Math.Ceiling(sizes[i] * DpiDetector.DpiXKoef);
+                _columnSizes.PutSizeOverride(i, Convert.ToInt32(pixelSize));
             }
 
             _columnSizes.BuildIndex();
